a9adc59fb7d54d7eb05e4a22eb322be35ee32b69
Lester Arteaga <lesterarte@gmail.com>
2019-06-12 07:19:28 -0600

    trabajando en login

--- js/users.js
+++ js/users.js
@@ -1,10 +1,14 @@
-function registrarUsuarios(){
+function logginAuth(){
     var email = document.getElementById('userAddNew').value;
     var pass = document.getElementById('passAddUser').value;
     
 
     firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
         // Handle Errors here.
+        
+        document.getElementById('userAddNew').value = ""; 
+        document.getElementById('passAddUser').value = "";
+        alert('Usuario o contraseña incorrecta');
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(error)
