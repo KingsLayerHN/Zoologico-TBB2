a1854ec08f7ea87b46c4e6f583099631a1d71fe9
Lester Arteaga <lesterarte@gmail.com>
2019-06-15 09:37:03 -0600

    changed loggin

--- js/users.js
+++ js/users.js
@@ -1,18 +1,38 @@
-function logginAuth(){
-    var email = document.getElementById('userAddNew').value;
-    var pass = document.getElementById('passAddUser').value;
-    
+function logginAuth() {
+  var email = document.getElementById('userAddNew').value;
+  var pass = document.getElementById('passAddUser').value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-        // Handle Errors here.
-        
-        document.getElementById('userAddNew').value = ""; 
-        document.getElementById('passAddUser').value = "";
-        alert('Usuario o contraseña incorrecta');
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(error)
-        // ...
-      });
-    
+  firebase.auth().signInWithEmailAndPassword(email, pass).then(function (user) {
+    if (user) {
+      window.location.href("./home.html");
+
+    }
+  }).catch(function (error) {
+    document.getElementById('userAddNew').value = "";
+    document.getElementById('passAddUser').value = "";
+    alert('Usuario o contraseña incorrecta');
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error)
+
+  });
+
+}
+
+function watcher() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      //user is signed in
+      alert('huy papa le huyo a mi tio frijol');
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+    } else {
+
+    }
+  });
 }
